df2a9b56bb41ee0e39d715147e2635a9a2c3eff2
Jagadeesh V <113669871+Jagadeesh260@users.noreply.github.com>
2025-01-27 18:13:56 +0530

    Add files via upload

---
+++ Bare metal programming ARM/8_uart_rx/Debug/Src/uart.d
@@ -0,0 +1,18 @@
+Src/uart.o: ../Src/uart.c ../Src/uart.h \
+ D:/MCU\ refe/Bare\ metal\ programming/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/MCU\ refe/Bare\ metal\ programming/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ D:/MCU\ refe/Additional\ files\ F4/Include/core_cm4.h \
+ D:/MCU\ refe/Additional\ files\ F4/Include/cmsis_version.h \
+ D:/MCU\ refe/Additional\ files\ F4/Include/cmsis_compiler.h \
+ D:/MCU\ refe/Additional\ files\ F4/Include/cmsis_gcc.h \
+ D:/MCU\ refe/Additional\ files\ F4/Include/mpu_armv7.h \
+ D:/MCU\ refe/Bare\ metal\ programming/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Src/uart.h:
+D:/MCU\ refe/Bare\ metal\ programming/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/MCU\ refe/Bare\ metal\ programming/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+D:/MCU\ refe/Additional\ files\ F4/Include/core_cm4.h:
+D:/MCU\ refe/Additional\ files\ F4/Include/cmsis_version.h:
+D:/MCU\ refe/Additional\ files\ F4/Include/cmsis_compiler.h:
+D:/MCU\ refe/Additional\ files\ F4/Include/cmsis_gcc.h:
+D:/MCU\ refe/Additional\ files\ F4/Include/mpu_armv7.h:
+D:/MCU\ refe/Bare\ metal\ programming/STM32Cube_FW_F4_V1.28.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
